a5148c9f01cdeacef569725209683a660f2f082a
Ihor Soloviov <igor.musson.55@gmail.com>
2023-07-03 16:27:22 +0300

    Loader was created

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Loader.scss'
+
+const Loader = () => {
+  return (
+    <section className='anim-inner'>
+      <div className="anim">
+      </div>
+    </section>
+  )
+}
+
+export default Loader
