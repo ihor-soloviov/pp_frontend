a7545c43f61283b4afde2fe8cd749e73b47bdb06
Ihor Soloviov <igor.musson.55@gmail.com>
2024-01-22 17:30:09 +0200

    + mob version

--- src/utils/useSmoothScroll.jsx
+++ src/utils/useSmoothScroll.jsx
@@ -3,16 +3,20 @@ import { useCallback } from "react";
 const useSmoothScroll = () => {
   const scrollTo = useCallback((id) => {
     const element = document.getElementById(id);
-    const header = document.querySelector('.header');
+    const header = document.querySelector(".header");
+    const isMobile = window.innerWidth < 1024;
 
     if (element) {
       const headerHeight = header ? header.offsetHeight : 0;
-      const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
-      const offsetPosition = elementPosition - headerHeight - 10; // 10px відступ від header
+      const elementPosition =
+        element.getBoundingClientRect().top + window.scrollY;
+      const offsetPosition = isMobile
+        ? elementPosition - headerHeight - 160
+        : elementPosition - headerHeight - 10; // 10px відступ від header
 
       window.scrollTo({
         top: offsetPosition,
-        behavior: 'smooth'
+        behavior: "smooth",
       });
     }
   }, []);
