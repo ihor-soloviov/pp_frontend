88a05173a1a082b4ebb81def63ebf9ef46be113f
Ihor Soloviov <igor.musson.55@gmail.com>
2024-02-22 14:09:28 +0200

    Refactor HeaderNavigation to use an array for navigation

--- src/components/Header/HeaderComponents/HeaderNavigation .jsx
+++ src/components/Header/HeaderComponents/HeaderNavigation .jsx
@@ -1,20 +1,22 @@
 import { NavLink } from "react-router-dom";
+const navigation = [
+  { href: '/', label: "Головна" },
+  { href: '/menu', label: "Меню" },
+  { href: '/about-us', label: "Про нас" },
+  { href: '/contact', label: "Контакти" },
+]
 
-export const HeaderNavigation = () => {
-  const navigation = [
-    { href: '/', label: "Головна" },
-    { href: '/menu', label: "Меню" },
-    { href: '/about-us', label: "Про нас" },
-    { href: '/contact', label: "Контакти" },
-  ]
-
-  return (
+export const HeaderNavigation = () => (
     <nav className="header__navigation">
       {navigation.map(({ href, label }) => (
-        <NavLink key={label} to={href} exact={label === "Головна"} className={({ isActive }) => isActive ? "navLink-active" : "navLink"}>
+        <NavLink
+          key={label}
+          to={href}
+          exact={label === "Головна"}
+          className={({ isActive }) => isActive ? "navLink-active" : "navLink"}
+        >
           {label}
         </NavLink>
       ))}
     </nav>
   );
-}
