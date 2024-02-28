66caba2af98b16f1585726f7d0d03416360ce97d
Serhii Savchak <intelekt200012@gmail.com>
2024-02-28 12:03:26 +0200

    add fix

--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -1,34 +1,24 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 //Import React
-import React, { useCallback, useState } from "react";
-
-import ProfileLink from "../ProfileLink/ProfileLink";
-import NumberChangeModal from "./NumberChangeModal";
-
-import axios from "axios";
-import { url } from "../../api";
-import { uploadImage } from "../../utils/firebase";
+import React, { useCallback, useState } from 'react';
+import Dropdown from 'react-dropdown';
+import ProfileLink from '../ProfileLink/ProfileLink';
+import NumberChangeModal from './NumberChangeModal';
+import sprite from '../../assets/images/sprite.svg';
+import axios from 'axios';
+import { url } from '../../api';
+import { uploadImage } from '../../utils/firebase';
 
 //Import Mobx
-import { observer } from "mobx-react-lite";
-import userStore from "../../store/user-store";
-
+import { observer } from 'mobx-react-lite';
+import userStore from '../../store/user-store';
+import { convertNumber } from '../../utils/convertNumber';
 //Import styles
-import "../ProfileGrid/ProfileGrid.scss";
-import "./InfoSection.scss";
+import '../ProfileGrid/ProfileGrid.scss';
+import './InfoSection.scss';
 
 const InfoSection = observer(({ handleSidebar, isSidebarClosed }) => {
-  const {
-    name,
-    email,
-    phone,
-    dateOfBirth,
-    token,
-    city,
-    setUserDataToStore,
-    avatar,
-  } =
-    userStore;
+  const { name, email, phone, dateOfBirth, token, city, setUserDataToStore, avatar } = userStore;
 
   const [formData, setFormData] = useState({
     name: name,
@@ -37,190 +27,177 @@ const InfoSection = observer(({ handleSidebar, isSidebarClosed }) => {
     city: city,
     token: token,
   });
+  const [cityDrop, setCityDrop] = useState('');
+  console.log('cityDrop', cityDrop);
+  const cityOptions = ['Одеса', 'Ужгород'].map((cityDrop) => ({
+    value: cityDrop,
+    label: cityDrop,
+    className: 'Dropdown-option',
+  }));
 
   const [isNumberChanging, setIsNumberChanging] = useState(false);
   const [file, setFile] = useState(undefined);
 
-  const handleChange = useCallback(
-    (e) => {
-      setFormData({ ...formData, [e.target.name]: e.target.value });
-    },
-    []
-  )
+  const handleChange = useCallback((e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }, []);
 
+  const handleSubmit = useCallback(async (e) => {
+    e.preventDefault();
 
-  const handleSubmit = useCallback(
-    async (e) => {
-      e.preventDefault();
+    try {
+      const response = await axios.put(`${url}/api/updateInfo/`, formData);
 
-      try {
-        const response = await axios.put(`${url}/api/updateInfo/`, formData);
-
-        if (response.status === 200) {
-          console.log(response.data);
-          setUserDataToStore({ ...response.data });
-        }
-
-        console.log(response);
-      } catch (error) {
-        console.error(error);
+      if (response.status === 200) {
+        console.log(response.data);
+        setUserDataToStore({ ...response.data });
       }
-    },
-    [],
-  )
 
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
 
   const openModal = () => {
-    setIsNumberChanging(true)
+    setIsNumberChanging(true);
   };
 
   if (!isSidebarClosed) {
     return (
-      <section
-        className="grid_layout--main profile_info"
-      >
-
-        <div className="profile_info--head">
-          <img
-            src={avatar}
-            alt="profile"
-            className="mobile-menu__avatar"
-            width={70}
-          />
-          <div className="profile_info--head__contacts contacts">
-            <div className="contacts_name">{name} </div>
-            <div className="contacts_phone">{phone}</div>
+      <section className='grid_layout--main profile_info'>
+        <div className='profile_info--head'>
+          <img src={avatar} alt='profile' className='mobile-menu__avatar' width={70} />
+          <div className='profile_info--head__contacts contacts'>
+            <div className='contacts_name'>{name} </div>
+            <div className='contacts_phone'>{phone}</div>
           </div>
-          <div className="profile_info--head__button button">
-            <button
-              className="button_link"
-              onClick={openModal}
-            >
+          <div className='profile_info--head__button button'>
+            <button className='button_link' onClick={openModal}>
               Змінити номер
             </button>
           </div>
-          <div className="profile_info--head__button button">
+          <div className='profile_info--head__button button'>
             <input
               disabled={!!file}
-              type="file"
-              id="fileInput"
-              style={{ display: "none" }}
+              type='file'
+              id='fileInput'
+              style={{ display: 'none' }}
               onChange={(e) => setFile(e.target.files[0])}
-              accept="image/png, image/jpeg, image/jpg, image/webp"
+              accept='image/png, image/jpeg, image/jpg, image/webp'
             />
-            <label htmlFor="fileInput" className="button_link">Завантажити фото</label>
+            <label htmlFor='fileInput' className='button_link'>
+              Завантажити фото
+            </label>
             {file && <button onClick={() => uploadImage(file)}>send</button>}
           </div>
         </div>
         <ProfileLink handleSidebar={handleSidebar}>Інформація</ProfileLink>
-        <div className="profile_info--head__mobile">
+        <div className='profile_info--head__mobile'>
           <img
-            src="https://via.placeholder.com/70x70"
-            alt="profile"
-            className="profile_info--head__photo"
+            src='https://via.placeholder.com/70x70'
+            alt='profile'
+            className='profile_info--head__photo'
           />
-          <div className="contacts">
-            <div className="contacts_name">{name}</div>
-            <div className="contacts_phone">
-              {phone}{" "}
-              <button
-                className="button_link"
-                onClick={openModal}
-              >
+          <div className='contacts'>
+            <div className='contacts_name'>{name}</div>
+            <div className='contacts_phone'>
+              {convertNumber(phone)}{' '}
+              <button className='button_link' onClick={openModal}>
                 Змінити
               </button>
             </div>
-            <div className="">
+            <div className=''>
               <input
                 disabled={file}
-                type="file"
-                id="fileInput"
-                style={{ display: "none" }}
+                type='file'
+                id='fileInput'
+                style={{ display: 'none' }}
                 onChange={(e) => setFile(e.target.files[0])}
-                accept="image/png, image/jpeg, image/jpg, image/webp"
+                accept='image/png, image/jpeg, image/jpg, image/webp'
               />
-              <label htmlFor="fileInput" className="button_link">Завантажити фото</label>
+              <label htmlFor='fileInput' className='button_link label'>
+                Завантажити фото
+              </label>
               {file && <button onClick={() => uploadImage(file)}>send</button>}
             </div>
           </div>
         </div>
-        <div className="profile_info--bonuses__mobile" />
-        <div className="profile_info--form">
+        <div className='profile_info--bonuses__mobile' />
+        <div className='profile_info--form'>
           <form onSubmit={handleSubmit}>
-            <div className="form">
-              <div className="form_item">
+            <div className='form'>
+              <div className='form_item'>
                 <p>Ім'я</p>
-                <label className="form_item--label">
+                <label className='form_item--label'>
                   <input
-                    type="text"
+                    type='text'
                     placeholder="Ім'я"
-                    name="name"
+                    name='name'
                     value={formData.name}
                     onChange={handleChange}
                   />
                 </label>
               </div>
 
-              <div className="form_item">
+              <div className='form_item'>
                 <p>Пошта</p>
-                <label className="form_item--label">
+                <label className='form_item--label'>
                   <input
-                    type="text"
-                    placeholder="xxx@gmail.com"
-                    name="email"
+                    type='text'
+                    placeholder='xxx@gmail.com'
+                    name='email'
                     value={formData.email}
                     onChange={handleChange}
                   />
                 </label>
               </div>
 
-              <div className="form_item">
+              <div className='form_item'>
                 <p>Дата народження</p>
-                <label className="form_item--label">
+                <label className='form_item--label'>
                   <input
-                    title="формат дд.мм.рррр"
-                    type="text"
-                    placeholder="Дата народження"
-                    name="dateOfBirth"
+                    title='формат дд.мм.рррр'
+                    type='text'
+                    placeholder='Дата народження'
+                    name='dateOfBirth'
                     value={formData.dateOfBirth}
                     onChange={handleChange}
                   />
                 </label>
               </div>
 
-              <div className="form_item">
+              <div className='form_item'>
                 <p>Місто</p>
-                <label className="form_item--label">
-                  <select
-                    name="city"
-                    value={formData.city}
-                    onChange={handleChange}
-                  >
-                    <option value="Оберіть місто" disabled>
-                      Оберіть місто
-                    </option>
-                    <option value="Одеса">Одеса</option>
-                    <option disabled value="Ужгород">
-                      Ужгород
-                    </option>
-                  </select>
+                <label className='form_item--label'>
+                  <Dropdown
+                    className='cityDrop'
+                    placeholder='Оберіть місто'
+                    options={cityOptions}
+                    value={cityDrop}
+                    onChange={setCityDrop}
+                    arrowClosed={
+                      <svg className='iconArrow'>
+                        <use href={sprite + '#icon-arrow'}></use>
+                      </svg>
+                    }
+                    arrowOpen={
+                      <svg className='iconArrowRotate'>
+                        <use href={sprite + '#icon-arrow'}></use>
+                      </svg>
+                    }
+                  />
                 </label>
               </div>
             </div>
-            <div className="form-button">
-              <button
-                className="btn-main "
-                type="submit"
-                style={{ justifyContent: "center" }}
-              >
+            <div className='form-button'>
+              <button className='btn-main ' type='submit' style={{ justifyContent: 'center' }}>
                 Зберегти зміни
               </button>
             </div>
           </form>
         </div>
-        {isNumberChanging && (
-          <NumberChangeModal setIsNumberChanging={setIsNumberChanging} />
-        )}
+        {isNumberChanging && <NumberChangeModal setIsNumberChanging={setIsNumberChanging} />}
       </section>
     );
   }
