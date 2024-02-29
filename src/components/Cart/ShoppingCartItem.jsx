5664dcf5b89cab418a34b0511853f38cf167d03f
Serhii Savchak <intelekt200012@gmail.com>
2024-02-29 19:59:08 +0200

    add fix to button

--- src/components/Cart/ShoppingCartItem.jsx
+++ src/components/Cart/ShoppingCartItem.jsx
@@ -1,61 +1,57 @@
-import React from "react";
-import shoppingCartStore from "../../store/shoping-cart-store";
-import { observer } from "mobx-react-lite";
+import React from 'react';
+import shoppingCartStore from '../../store/shoping-cart-store';
+import { observer } from 'mobx-react-lite';
 
 const ShoppingCartItem = observer(({ id }) => {
-
   const { removeFromCart, getItemById, updateItemQuantity } = shoppingCartStore;
   const cartItem = getItemById(id);
 
-  if (!cartItem) return null
+  if (!cartItem) return null;
 
-  const { preview, name, weight, price, count, mods, cartItemId } = cartItem
+  const { preview, name, weight, price, count, mods, cartItemId } = cartItem;
 
   return (
-    <li className="shopping-cart__item">
-      <div className="item-inner">
-        <div className="shopping-cart__preview">
-          <img src={preview} alt="product" />
+    <li className='shopping-cart__item'>
+      <div className='item-inner'>
+        <div className='shopping-cart__preview'>
+          <img src={preview} alt='product' />
         </div>
-        <div className="shopping-cart__info">
-          <div className="shopping-cart__row">
-            <h6 className="title__h8">{name}</h6>
-            <div
-              className="shopping-cart__remove"
-              onClick={() => removeFromCart(cartItemId)}
-            >
+        <div className='shopping-cart__info'>
+          <div className='shopping-cart__row'>
+            <h6 className='title__h8'>{name}</h6>
+            <div className='shopping-cart__remove' onClick={() => removeFromCart(cartItemId)}>
               <svg
-                width="12"
-                height="12"
-                viewBox="0 0 12 12"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+                width='12'
+                height='12'
+                viewBox='0 0 12 12'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
               >
                 <path
-                  d="M6.0006 4.82144L10.1256 0.696442L11.3039 1.87478L7.17893 5.99977L11.3039 10.1248L10.1256 11.3031L6.0006 7.17811L1.8756 11.3031L0.697266 10.1248L4.82227 5.99977L0.697266 1.87478L1.8756 0.696442L6.0006 4.82144Z"
-                  fill="#49484D"
+                  d='M6.0006 4.82144L10.1256 0.696442L11.3039 1.87478L7.17893 5.99977L11.3039 10.1248L10.1256 11.3031L6.0006 7.17811L1.8756 11.3031L0.697266 10.1248L4.82227 5.99977L0.697266 1.87478L1.8756 0.696442L6.0006 4.82144Z'
+                  fill='#49484D'
                 />
               </svg>
             </div>
           </div>
 
-          <p className="shopping-cart__weight">{weight} г</p>
-          <div className="shopping-cart__row">
-            <p className="shopping-cart__price">{price} ₴</p>
-            <div className="counter">
+          <p className='shopping-cart__weight'>{weight} г</p>
+          <div className='shopping-cart__row'>
+            <p className='shopping-cart__price'>{price} ₴</p>
+            <div className='counter'>
               <div
-                className="counter__btn counter__btn--light"
+                className='counter__btn counter__btn--light'
                 onClick={() => {
                   if (count > 1) {
-                    updateItemQuantity(cartItemId, count - 1)
+                    updateItemQuantity(cartItemId, count - 1);
                   }
                 }}
               >
                 -
               </div>
-              <div className="counter__value">{count}</div>
+              <div className='counter__value'>{count}</div>
               <div
-                className="counter__btn counter__btn--light"
+                className='counter__btn counter__btn--light'
                 onClick={() => updateItemQuantity(cartItemId, count + 1)}
               >
                 +
@@ -64,10 +60,10 @@ const ShoppingCartItem = observer(({ id }) => {
           </div>
         </div>
       </div>
-      {!!mods.length && (
-        <ul className="mods-inner">
-          {mods.map(mod => (
-            <li className="mods-item" key={mod.m}>
+      {mods && mods?.length !== 0 && (
+        <ul className='mods-inner'>
+          {mods.map((mod) => (
+            <li className='mods-item' key={mod.m}>
               <p>+{mod.name}</p>
               <p>+{mod.price}₴</p>
             </li>
@@ -76,7 +72,6 @@ const ShoppingCartItem = observer(({ id }) => {
       )}
     </li>
   );
-}
-);
+});
 
 export default ShoppingCartItem;
