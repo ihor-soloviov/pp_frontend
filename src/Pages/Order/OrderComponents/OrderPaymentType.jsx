07eca7f927fa4a6f1442f184058be3767b4ea024
Serhii Savchak <intelekt200012@gmail.com>
2024-03-01 10:05:41 +0200

    add custom time slelect

--- src/Pages/Order/OrderComponents/OrderPaymentType.jsx
+++ src/Pages/Order/OrderComponents/OrderPaymentType.jsx
@@ -1,46 +1,45 @@
-import React, { useState } from 'react'
-import InputText from '../../../components/Inputs/InputText'
+import React, { useState } from 'react';
+import InputText from '../../../components/Inputs/InputText';
 import { CustomSelect } from '../../../components/CustomSelect/CustomSelect';
 
 export const OrderPaymentType = ({ formData, handleFormValueChange }) => {
   const { bonus, change } = formData;
-  const [payment, setPayment] = useState({label: "Онлайн", value: "Онлайн"})
-
+  const [payment, setPayment] = useState({ label: 'Онлайн', value: 'Онлайн' });
 
   const handleChangePayment = (e) => {
-    setPayment(e)
-    handleFormValueChange("paymentMethod", e)
-  }
+    setPayment(e);
+    handleFormValueChange('paymentMethod', e.value);
+  };
 
   return (
-    <section className="order-page__section">
+    <section className='order-page__section'>
       <h3 className='order-page__header'>Спосіб оплати</h3>
-      <div className="order-page__section-inputs">
+      <div className='order-page__section-inputs'>
         <InputText
-          name={"Використати бонуси"}
-          placeholder={"0"}
+          name={'Використати бонуси'}
+          placeholder={'0'}
           value={bonus}
-          onChange={(value) => handleFormValueChange("bonus", value)}
+          onChange={(value) => handleFormValueChange('bonus', value)}
         />
       </div>
-      <div className="order-page__section-inputs order-page__section-inputs-row">
+      <div className='order-page__section-inputs order-page__section-inputs-row'>
         <label className='inputText'>
           <span>Промокод</span>
           <CustomSelect
             className={`cityDrop promo`}
             placeholder='Онлайн'
             value={payment}
-            options={["Онлайн", "Готівка"]}
-            handleChange={handleChangePayment} />
+            options={['Онлайн', 'Готівка']}
+            handleChange={handleChangePayment}
+          />
         </label>
         <InputText
-          name={"Сдача с"}
-          placeholder={"500"}
+          name={'Сдача с'}
+          placeholder={'500'}
           value={change}
-          onChange={(value) => handleFormValueChange("change", value)}
+          onChange={(value) => handleFormValueChange('change', value)}
         />
       </div>
     </section>
-  )
-}
-
+  );
+};
--- src/Pages/Order/OrderComponents/OrderTime.jsx
+++ src/Pages/Order/OrderComponents/OrderTime.jsx
@@ -1,42 +1,52 @@
-import React from 'react'
-import RadioButton from '../../../components/RadioButton/RadioButton'
-import InputSelector from '../../../components/Inputs/InputSelector'
-import { timeArray } from '../time'
+import React from 'react';
+import RadioButton from '../../../components/RadioButton/RadioButton';
+import { timeArray } from '../time';
 import { filterTimeArray } from '../OrderFunctions/OrderTools';
-
-const time = filterTimeArray(timeArray);
+import { CustomSelect } from '../../../components/CustomSelect/CustomSelect';
+import { useState } from 'react';
+import '../../../components/Inputs/InputText.scss';
+const timeOptions = filterTimeArray(timeArray);
 
 export const OrderTime = React.memo(({ handleFormValueChange, formData }) => {
-  const { deliveryTime, selectedTime } = formData
+  const { deliveryTime } = formData;
+
+  const [dropTime, setDropTime] = useState(null);
+
+  const handleChangeTime = (e) => {
+    setDropTime(e.value);
+    handleFormValueChange('selectedTime', e.value);
+  };
+
   return (
-    <section className="order-page__section">
+    <section className='order-page__section'>
       <h3 className='order-page__header'>Час отримання</h3>
-      <section className="order-page__section-inputs">
+      <section className='order-page__section-inputs'>
         <RadioButton
           data={[
-            { id: 1, value: "На зараз", label: "На зараз" },
+            { id: 1, value: 'На зараз', label: 'На зараз' },
             {
               id: 2,
-              value: "Вказати точний час",
-              label: "Вказати точний час",
+              value: 'Вказати точний час',
+              label: 'Вказати точний час',
             },
           ]}
           selectedOption={deliveryTime}
-          onOptionChange={(event) =>
-            handleFormValueChange("deliveryTime", event.target.value)
-          }
+          onOptionChange={(event) => handleFormValueChange('deliveryTime', event.target.value)}
           column
         />
-        {deliveryTime === "Вказати точний час" && (
-          <InputSelector
-            name={"Час"}
-            placeholder={"Час"}
-            data={time}
-            value={selectedTime}
-            onChange={(value) => handleFormValueChange("selectedTime", value)}
-          />
+        {deliveryTime === 'Вказати точний час' && (
+          <label className='inputText'>
+            <span>{'Час'}</span>
+            <CustomSelect
+              className={'cityDrop time'}
+              placeholder={'Час'}
+              options={timeOptions}
+              value={dropTime && dropTime}
+              handleChange={handleChangeTime}
+            />
+          </label>
         )}
       </section>
     </section>
-  )
-})
+  );
+});
