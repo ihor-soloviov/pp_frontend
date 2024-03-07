5a340b0c6f2bb21967cdccc6b31baa9b82eee499
Serhii Savchak <itsavchak@gmail.com>
2024-03-07 18:37:54 +0200

    add rremove by id , add by id functional

--- src/components/CreatedAddress/CreatedAddress.jsx
+++ src/components/CreatedAddress/CreatedAddress.jsx
@@ -1,47 +1,55 @@
-import React from "react";
-import cross from "../../../src/assets/Vector.svg";
-import userStore from "../../store/user-store";
-import { deleteAddress } from "../../utils/addresses";
-import { observer } from "mobx-react-lite";
-import { getFlat, getStreet } from "./utils";
+import React from 'react';
+import cross from '../../../src/assets/Vector.svg';
+import userStore from '../../store/user-store';
+import { deleteAddress } from '../../utils/addresses';
+import { observer } from 'mobx-react-lite';
+import { getFlat, getStreet } from './utils';
 
 const CreatedAddress = observer(
-  ({ adress, openModal, setIsAdressesUpdating }) => {
+  ({ adress, openModal, setIsAdressesUpdating, isEdit, setIsEdit, setCurrentAddressId }) => {
     const { token, removeAdresses } = userStore;
 
     const flat = getFlat(adress);
     const street = getStreet(adress.streetName, adress.homeNumber, flat);
 
     return (
-      <div className="addresses_created">
-        <div className="addresses_created__container">
-          <div className="addresses_created__container--top">
-            <div className="top-name">
+      <div id={adress.addressId} className='addresses_created'>
+        <div className='addresses_created__container'>
+          <div className='addresses_created__container--top'>
+            <div className='top-name'>
               <h4>{adress.adressName}</h4>
               <button
                 onClick={() => {
-                  removeAdresses(adress.adressName);
+                  removeAdresses(adress.addressId);
                   deleteAddress(token, adress, setIsAdressesUpdating);
                 }}
               >
-                <img src={cross} alt="cross" />
+                <img src={cross} alt='cross' />
               </button>
             </div>
-            <div className="top-street">
+            <div className='top-street'>
               <p>
                 <span>Вулиця</span> {street}
               </p>
             </div>
           </div>
-          <div className="addresses_created__container--bot">
+          <div className='addresses_created__container--bot'>
             <span>Комментар:</span>
             <p>{adress.comment}</p>
-            <button onClick={openModal}>Редагувати</button>
+            <button
+              onClick={() => {
+                setIsEdit(!isEdit);
+                setCurrentAddressId(adress.addressId);
+                openModal();
+              }}
+            >
+              Редагувати
+            </button>
           </div>
         </div>
       </div>
     );
-  }
+  },
 );
 
 export default CreatedAddress;
--- src/utils/addresses.js
+++ src/utils/addresses.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { url } from "../api";
+import axios from 'axios';
+import { url } from '../api';
 
 export const deleteAddress = async (token, adress, setIsAdressesUpdating) => {
   try {
@@ -7,13 +7,31 @@ export const deleteAddress = async (token, adress, setIsAdressesUpdating) => {
       token: token,
       data: adress,
     });
+
     await axios.post(`${url}/api/deleteAddress`, JSONrequest, {
       headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
       },
     });
-    setIsAdressesUpdating(prev => !prev);
+    setIsAdressesUpdating((prev) => !prev);
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const updateAddress = async (token, adress) => {
+  try {
+    const JSONrequest = JSON.stringify({
+      token: token,
+      address: adress,
+    });
+
+    await axios.patch(`${url}/api/updateAdress`, JSONrequest, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+    });
   } catch (error) {
     console.log(error);
   }
