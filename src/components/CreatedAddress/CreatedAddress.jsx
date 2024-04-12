649b37869a0424dd9ab0c6c78c3f5e731446da59
Serhii Savchak <itsavchak@gmail.com>
2024-04-12 15:32:33 +0300

    add fix to addres

--- src/components/CreatedAddress/CreatedAddress.jsx
+++ src/components/CreatedAddress/CreatedAddress.jsx
@@ -7,21 +7,21 @@ import { observer } from 'mobx-react-lite';
 const CreatedAddress = observer(
   ({ adress, openModal, setIsAdressesUpdating, isEdit, setIsEdit, setCurrentAddressId }) => {
     const { token, removeAdresses } = userStore;
-    const street = adress.address;
+    const street = adress?.address;
 
     return (
-      <div id={adress.addressId} className='addresses_created'>
+      <div id={adress?.addressId} className='addresses_created'>
         <div className='addresses_created__container'>
           <div className='addresses_created__container--top'>
             <div className='top-name'>
               <h4>
-                {adress.adressName.length < 14
-                  ? adress.adressName
-                  : `${adress.adressName.slice(0, 15)}...`}
+                {adress?.adressName?.length < 14
+                  ? adress?.adressName
+                  : `${adress?.adressName.slice(0, 15)}...`}
               </h4>
               <button
                 onClick={() => {
-                  removeAdresses(adress.addressId);
+                  removeAdresses(adress?.addressId);
                   deleteAddress(token, adress, setIsAdressesUpdating);
                 }}
               >
@@ -29,18 +29,18 @@ const CreatedAddress = observer(
               </button>
             </div>
             <div className='top-street'>
-              <p>{street.length < 33 ? street : `${street.slice(0, 33)}...`}</p>
+              <p>{street?.length < 33 ? street : `${street.slice(0, 33)}...`}</p>
             </div>
           </div>
           <div className='addresses_created__container--bot'>
             <span>Комментар:</span>
             <p>
-              {adress.comment.length < 25 ? adress.comment : `${adress.comment.slice(0, 25)}...`}
+              {adress?.comment?.length < 25 ? adress.comment : `${adress.comment.slice(0, 25)}...`}
             </p>
             <button
               onClick={() => {
                 setIsEdit(!isEdit);
-                setCurrentAddressId(adress.addressId);
+                setCurrentAddressId(adress?.addressId);
                 openModal();
               }}
             >
