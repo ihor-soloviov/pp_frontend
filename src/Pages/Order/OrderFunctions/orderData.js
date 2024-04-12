e5d247946eb0389c853e44ea76377bfaa9c46530
Serhii Savchak <itsavchak@gmail.com>
2024-04-12 18:02:57 +0300

    Update orderData.js

--- src/Pages/Order/OrderFunctions/orderData.js
+++ src/Pages/Order/OrderFunctions/orderData.js
@@ -53,7 +53,9 @@ export const getValidateRules = (formData, cartItems, totalPrice, deliveryPrice)
     { check: () => cartItems.length === 0, message: 'Будь ласка, оберіть товари для замовлення' },
     {
       check: () =>
-        !/^\+380 \((50|63|66|67|68|91|92|93|94|95|96|97|98|99)\) \d{3} \d{2} \d{2}$/.test(number),
+        !/^\+380 \((39|50|63|66|67|73|68|91|92|93|94|95|96|97|98|99)\) \d{3} \d{2} \d{2}$/.test(
+          number,
+        ),
       message: 'Будь ласка, введіть коректний номер телефону',
     },
     { check: () => number === '', message: 'Будь ласка, заповніть поле номеру телефону' },
