da7300a1bb4c3bd8408317ebca1db3799b0aa44d
Ihor Soloviov <igor.musson.55@gmail.com>
2024-04-18 14:03:17 +0300

    Add MetaPixel component for Facebook Pixel tracking

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ import { BrowserRouter } from "react-router-dom";
 //Import styles
 import "./style/index.scss";
 import "./firebaseConfig";
+import MetaHelmet from "./components/MetaPixel/MetaPixel";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <MetaHelmet />
     <BrowserRouter>
       <App />
     </BrowserRouter>
